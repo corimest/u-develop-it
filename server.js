a5397d610a75b19f701d21918c857bf872828b5b
Corinne Mestemacher <corimest@Corinnes-MacBook-Air.local>
2022-07-04 17:10:29 -0500

    MySQL server connections

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+// import mySQL
+const mysql = require('mysql2'); 
+//import Express.js
 const express = require('express'); 
+// PORT server connections
 const PORT = process.env.PORT || 3001; 
 const app = express(); 
 
@@ -6,6 +10,23 @@ const app = express();
 app.use(express.urlencoded({ extended: false })); 
 app.use(express.json()); 
 
+// connect app to mySQL database
+const db = mysql.createConnection(
+    {
+        host: 'localhost', 
+        //MySQL username
+        user: 'root', 
+        // MySQL password
+        password: 'BelletheFrenchie2021!', 
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// SQL database query
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows); 
+}); 
 
 
 // Default response for requests that aren't supported by the app
