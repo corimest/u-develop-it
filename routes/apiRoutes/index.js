55cbe9eb3ef11d4194ff6bd213a41e840806c71e
Corinne Mestemacher <corimest@Corinnes-MacBook-Air.local>
2022-07-09 17:05:29 -0500

    modularization with original code commented out for notes and reference later

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,8 @@
+// Central hub to pull all routes together
+
+const express = require('express');
+const router = express.Router();
+
+router.use(require('./candidateRoutes'));
+
+module.exports = router;
